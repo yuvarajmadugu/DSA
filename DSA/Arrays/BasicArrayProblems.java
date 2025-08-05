4cfa94e2b065bf2d934460d31fbcf86fb43da598
yuvarajmadugu <yuvaraj6207@gmail.com>
2025-08-05 15:04:12 +0530

    add files

---
+++ DSA/Arrays/BasicArrayProblems.java
@@ -0,0 +1,17 @@
+package DSA.Arrays;
+
+public class BasicArrayProblems {
+    public static void main(String[] args) {
+
+////        find max in array:
+        int[] arr1 = new int[] {1,2,4,5,6,7,8,9};
+//        int max = arr1[0];
+//        for(int i=1; i<arr1.length; i++){
+//            if(arr1[i] > max) max = arr1[i];
+//        }
+//        System.out.println(max);
+
+
+
+    }
+}
