fc92b2b6131b37f4b05567152b150bcdd3866129
Yuvaraj Madugu <yuvaraj6207@gmail.com>
2025-08-11 20:53:07 +0530

    Add files via upload

---
+++ Arrays/LinearSearchTypes/TranspostionLS.java
@@ -0,0 +1,6 @@
+package DSA.Arrays.LinearSearchTypes;
+
+public class TranspostionLS {
+//// this is a type of Improved Linear search an optimizing technique
+//// basic idea of this technique swap found elements with prior element 
+}
